49eb9afdcf6da63334fa5e3ac8fec7367032793e
Steven Meyer <Triadarcher@gmail.com>
2019-03-06 10:48:13 -0800

    js changes

--- js/formcalculations.js
+++ js/formcalculations.js
@@ -1,59 +1,47 @@
-// //The values represent the cost of the pizza
-//  var cake_prices = new Array();
-//  cake_prices["Personal"]=6;
-//  cake_prices["Medium"]=10;
-//  cake_prices["Large"]=14;
-//  cake_prices["Extralarge"]=16;
- 
-
-//  var filling_prices= new Array();
-//  veggie_prices["Mushrooms"]=1;
-//  veggie_prices["Jalapeno"]=1;
-//  veggie_prices["Tomatoes"]=1;
-//  veggie_prices["Onions"]=1;
-//  veggie_prices["Olives"]=1;
-//  veggie_prices["Green Peppers"]=1;
-
-	 
-// function getPizzaSizePrice()
-// {  
-//     var pizzaSizePrice=0;
-//     var theForm = document.forms["pizzaform"];
-//     var selectedPizza = theForm.elements["pizzasize"];
-    
-//     for(var i = 0; i < selectedPizza.length; i++)
-//     {
-//         if(selectedPizza[i].checked)
-//         {
-
-//             pizzaSizePrice = pizza_prices[selectedPizza[i].value];
-
-//             break;
-//         }
-//     }
-   
-//     return pizzaSizePrice;
-// }
-
-
-
-
 
 function calculateTotal()
 {
     var sizeSelection = getSizeSelection();
     var veggieSelection = getVeggieSelection();
     var basePrice = getBasePrice();
-    console.log(basePrice)
+    console.log(veggieSelection)
 }
-
-
 
 function getSizeSelection(){
     var sizes = document.getElementsByClassName("pizzasize");
     for (let i = 0; i<sizes.length; i++){
         if (sizes[i].checked==true){
             var baseSelection = sizes[i].value;
+        }
+    };
+    return baseSelection;
+}
+
+function getCrustSelection(){
+    var crust = document.getElementsByClassName("crust");
+    for (let i = 0; i<sizes.length; i++){
+        if (crust[i].checked==true){
+            var baseSelection = crust[i].value;
+        }
+    };
+    return baseSelection;
+}
+
+function getCheeseSelection(){
+    var cheese = document.getElementsByClassName("cheese");
+    for (let i = 0; i<sizes.length; i++){
+        if (cheese[i].checked==true){
+            var baseSelection = cheese[i].value;
+        }
+    };
+    return baseSelection;
+}
+
+function getSauceSelection(){
+    var sauce = document.getElementsByClassName("sauce");
+    for (let i = 0; i<sizes.length; i++){
+        if (sauce[i].checked==true){
+            var baseSelection = sauce[i].value;
         }
     };
     return baseSelection;
@@ -70,12 +58,65 @@ function getVeggieSelection(){
     return veggieSelection;
 }
 
+function getMeatsSelection(){
+    var meats = document.getElementsByClassName("meats");
+    var meatsSelection = [];
+    for (let i=0; i<meats.length; i++) {
+        if(meats[i].checked == true){
+            meatsSelection.push(meats[i].value);
+        }
+    };
+    return veggieSelection;
+}
 function getBasePrice(){
     var selection = getSizeSelection();
     if(selection == "Personal"){return 6};
     if(selection == "Medium"){return 10};
     if(selection == "Large"){return 14};
     if(selection == "Extralarge"){return 16};
-    return null;
+    return null;}
 
-}
+function getBasePrice(){
+    var selection = getCrustSelection();
+    if(selection == "Plain Crust"){return 0};
+    if(selection == "Garlic Butter Crust"){return 0};
+    if(selection == "Spicy Crust"){return 0};
+    if(selection == "House Special Crust"){return 0};
+    if(selection == "Stuffed Crust"){return 3};
+    return null;}
+
+function getBasePrice(){
+    var selection = getVeggieSelection();
+    if(selection == "Mushrooms"){return 1};
+    if(selection == "Jalapeno"){return 1};
+    if(selection == "Tomatoes"){return 1};
+    if(selection == "Onions"){return 1};
+    if(selection == "Olives"){return 1};
+    if(selection == "Green Peppers"){return 1};
+    return null;}
+
+function getBasePrice(){
+    var selection = getMeatSelection();
+    if(selection == "Pepperoni"){return 1};
+    if(selection == "Sausage"){return 1};
+    if(selection == "Canadian Bacon"){return 1};
+    if(selection == "Ground Beef"){return 1};
+    if(selection == "Anchovy"){return 1};
+    if(selection == "Chicken"){return 1};
+    return null;}
+
+function getBasePrice(){
+    var selection = getCheeseSelection();
+    if(selection == "Regular Cheese"){return 0};
+    if(selection == "Extra Cheese"){return 3};
+    if(selection == "No Cheese"){return 0};
+    return null;}
+
+function getBasePrice(){
+    var selection = getSauceSelection();
+    if(selection == "Marinara Sauce"){return 0};
+    if(selection == "White Sauce"){return 0};
+    if(selection == "BBQ Sauce"){return 0};
+    if(selection == "No Sauce"){return 0};
+    return null;}
+
